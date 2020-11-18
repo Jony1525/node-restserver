c743a8e7f26af8ebf1a78228f83d5f23761940e6
Juan Luna <jony.luna15@gmail.com>
2020-11-17 18:10:10 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'env') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jony:Mnolose15@cluster0.4w7l7.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
