a18138218f8b1f04991ec80aa5819286f32f9e9d
Juan Luna <jony.luna15@gmail.com>
2020-11-18 23:24:12 -0600

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -13,12 +13,32 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ====================
+// Entorno
+// ====================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 60;
+
+
+
+
+// ====================
+// Seed
+// ====================
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+
+// ====================
 // Base de Datos
 // ====================
 
 let urlDB;
 
-if ( process.env.NODE_ENV === 'env') {
+if ( process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 const app = express();
 
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken, function (req, res) {
     
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -23,7 +24,6 @@ app.get('/usuario', function (req, res) {
                         err
                     });
                 }
-
                 Usuario.countDocuments({ estado: true }, (err, conteo) => {
                     res.json({
                         ok: true,
@@ -31,12 +31,11 @@ app.get('/usuario', function (req, res) {
                         cuantos: conteo
                     });
                 });
-
             });
 
 });
   
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function (req, res) {
   
     let body = req.body;
 
@@ -48,14 +47,14 @@ app.post('/usuario', function (req, res) {
     });
 
     usuario.save( (err, usuarioDB ) => {
-        
+
         if ( err ) {
             return res.status(400).json({
                 ok: false,
                 err
             });
         }
-
+        
         res.json({
             ok: true,
             usuario: usuarioDB
@@ -63,7 +62,7 @@ app.post('/usuario', function (req, res) {
     });
 });
   
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function (req, res) {
       
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -85,7 +84,7 @@ app.put('/usuario/:id', function (req, res) {
 
 });
   
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', verificaToken, function (req, res) {
     let id = req.params.id;
 
     let body = {
